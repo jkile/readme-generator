b2ed81d697d4df6bb361010e1245f370fa2d95eb
Jake Kile <jkile9@gmail.com>
2020-02-26 20:09:02 -0700

    initialized api call url and imported axios

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,10 @@
+const axios = require("axios");
+
 const api = {
   getUser(username) {
-
+    axios.get(`https://api.github.com/search/users?q=${username}`).then(function({data}){
+      
+    })
   }
 };
 
