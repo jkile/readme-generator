f24051dfb76021902775d889b38e6a58084734f7
Jake Kile <jkile9@gmail.com>
2020-02-26 21:19:47 -0700

    added questions for prompts and assigned data from prompts and api to variables

--- index.js
+++ index.js
@@ -6,7 +6,38 @@ const questions = [
     {
         type: "input",
         name: "username",
-        message: "Please provide your Github username: "
+        message: "Provide your Github username: "
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "Provide project name: "
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Provide license for project: ",
+        choices: ["APACHE 2.0", "GPL 3.0", "BSD 3", "MIT"]
+    },
+    {
+        type: "input",
+        name: "dependency",
+        message: "Provide command to run dependency install: "
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Provide command to run tests: "
+    },
+    {
+        type: "input",
+        name: "info",
+        message: "Provide details on use of repo: "
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Provide details on contributing to repo: "
     }
 
 ];
@@ -16,9 +47,11 @@ function writeToFile(fileName, data) {
 }
 
 async function init() {
-    const {username} = await inquirer.prompt(questions);
-    const {data} = await API.getUser(username);
-    console.log(data.items[0]);
+    const data = await inquirer.prompt(questions);
+    //const {data} = await API.getUser(username);
+    //console.log(data.items[0]);
+    console.log(data);
+
 }
 
 init();
